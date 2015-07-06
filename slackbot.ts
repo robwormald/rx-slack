b18c631947c4f5d7f65ed1dfde2a4fa822c7af49
Rob Wormald <robwormald@gmail.com>
2015-07-05 22:14:01 -0700

    basic functionality working

--- slackbot.ts
+++ slackbot.ts
@@ -1,12 +1,13 @@
 /// <reference path="typings/rx/rx" />
 /// <reference path="typings/ws/ws" />
 
+
 import Rx = require('rx');
 import RxNode = require('rx-node');
 import Fetch = require('node-fetch');
 import ws = require('ws');
 
-const SLACK_URL = (method, token) => `https://slack.com/api/${method}?token=${token}`
+const getSlackUrl = (method, token) => `https://slack.com/api/${method}?token=${token}`
 
 const getBotUrl = (slackDetails) => {
     console.log(slackDetails.url);
@@ -14,15 +15,55 @@ const getBotUrl = (slackDetails) => {
 }
     
 
-function connect(token){
-    let startConnection = Fetch(SLACK_URL('rtm.start',token));
-    return Rx.Observable.fromPromise(startConnection.then((res) => res.json()));
+const startConnection = (token) => {
+    return Rx.Observable
+        .fromPromise(Fetch(getSlackUrl('rtm.start', token)))
+        .flatMap((res) => res.json())
+        .map(getBotUrl);
 }
 
-connect(process.env.SLACK_TOKEN)
-  .flatMapLatest(getBotUrl)
-  .subscribe(function(res){
-    console.log(res);
-},function(err){
-    console.log(err)
-})
+const createSlackSocket = (url, openObserver?:Rx.Observer<any>, closeObserver?:Rx.Observer<any>) => {
+    //create socket
+    const socket = new ws(url);
+    
+    //handler for connection opening
+    const openHandler = (event) => {
+        openObserver.onNext(event);
+        openObserver.onCompleted();
+        socket.removeListener('open', openHandler);
+    }
+    
+    //incoming messages 
+    const messageStream = Rx.Observable.create((gen) => {
+        
+        const messageHandler = (message) => gen.onNext(message.data);
+        const errorHandler = (error) => gen.onError(error);
+        const closeHandler = (event) => gen.onCompleted();
+        
+        openObserver && socket.addEventListener('open',openHandler);
+        
+        socket.addEventListener('message', messageHandler);
+        socket.addEventListener('error',errorHandler);
+        socket.addEventListener('close',closeHandler);
+        
+        return () => {
+            socket.removeListener('message',messageHandler);
+            socket.removeListener('error',errorHandler);
+            socket.removeListener('close',closeHandler);
+        }
+        
+    });
+    
+    const responseStream = Rx.Observer.create((message) => {
+        socket.send(message);
+    })
+    
+    return Rx.Subject.create(responseStream, messageStream);
+}
+
+let slackStream = startConnection(process.env.SLACK_TOKEN)
+  .flatMap(createSlackSocket)
+  
+  slackStream.subscribe((ev) => {
+      console.log(ev)
+  })
