9bbb89d4e95061a4765118dce1f116b995e27fd1
Rob Wormald <robwormald@gmail.com>
2015-07-11 23:42:33 -0700

    starting on commands

--- slackbot.ts
+++ slackbot.ts
@@ -1,26 +1,73 @@
-import {SlackSocket} from './lib/SlackSocket'
+import {SlackSocket} from './lib/SlackSocket';
+import es6shim = require('es6-shim');
 
-console.log(SlackSocket)
+(es6shim)
+
+console.log('starting slackbot...', process.env.SLACK_TOKEN);
 
 
 let connectionStream = SlackSocket.create(process.env.SLACK_TOKEN);
 
+let commands = {};
+
 let slackSocket;
 
-let botMessageStream = connectionStream
+var count = 1;
+let messageStream = connectionStream
   .flatMap((socket) => {
+    console.log('opened connection...')
     slackSocket = socket;
-    return socket.mentions;
-  })
-  .forEach((message) => {
-    console.log('message',message)
-    slackSocket.send({
-      id: 1,
-      type: 'message',
-      channel: message.channel,
-      text: 'hello world'
-    })
-  })
+    return socket.messages.map((message) => ({
+      reply(text, mention){
+        socket.send({
+          type: 'message',
+          channel: message.channel,
+          text: `<@${mention || message.user}>: ${text}`,
+          id: count++
+        });
+      },
+      message
+    }))
+  });
   
+let commandStream = messageStream.filter((event) => event.message.text.startsWith("!")).publish();
+  
+ 
+let rememberCommandStream = commandStream.filter((event) => event.message.text.startsWith("!remember ")); 
+
+let execCommandStream = commandStream.filter((event) => {
+  var valid = !event.message.text.startsWith("!remember ");
+  console.log(valid);
+  return valid;
+});
+
+rememberCommandStream.subscribe((event) => {
+  let [remember, name, ...text] = event.message.text.split(" ");
+  
+  if(commands[name]){
+    event.reply('I already know about that :(');
+    return;
+  }
+  
+  commands[name] = text.join(' ');
+  event.reply(`OK, i'll remember that ${name} means '${commands[name]}'`);
+  
+});
+
+
+execCommandStream.subscribe((event) => {
+  console.log('command',event)
+  let [command, mention] = event.message.text.split(" ");
+  command = command.slice(1);
+  if(!commands[command]){
+    event.reply(`I don't know anything about that, sorry :( `);
+    return;
+  }
+  event.reply(commands[command],mention);
+})
+
+commandStream.connect();
+ 
+
 
 
